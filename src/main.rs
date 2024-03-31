0143bed78fbdc079b3cd7508193e2902f0761156
haku <gentlebuuny@gmail.com>
2024-03-31 12:54:19 +0900

    feat: implement email client

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,16 @@
-use std::net::TcpListener;
-
-use sqlx::postgres::PgPoolOptions;
-
 use zero2prod::configuration::get_configuration;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let configuration = get_configuration().expect("Failed to read configuration.");
-
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
-    let address = format!(
-        "{host}:{port}",
-        host = configuration.application.host,
-        port = configuration.application.port,
-    );
-
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
-
     // env
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    run(listener, connection_pool)?.await
+    let config = get_configuration().expect("Failed to read configuration.");
+    let app = Application::build(config).await?;
+    app.run_until_stopped().await?;
+
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,74 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
+use crate::configuration::{DatabaseSettings, Settings};
+use crate::email_client::EmailClient;
 use crate::routes::{health, subscribe};
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+pub fn get_connection_pool(config: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(config.with_db())
+}
+
+impl Application {
+    pub async fn build(config: Settings) -> Result<Self, std::io::Error> {
+        // database
+        let connection_pool = get_connection_pool(&config.database);
+
+        // email client
+        let sender_email = config
+            .email_client
+            .sender()
+            .expect("Invalid sender email address.");
+        let timeout = config.email_client.timeout();
+        let email_client = EmailClient::new(
+            config.email_client.base_url,
+            sender_email,
+            config.email_client.authorization_token,
+            timeout,
+        );
+
+        // server
+        let address = format!(
+            "{host}:{port}",
+            host = config.application.host,
+            port = config.application.port,
+        );
+        let listener = TcpListener::bind(address).expect("Failed to bind random port");
+        let port = listener.local_addr().unwrap().port();
+
+        let server = run(listener, connection_pool, email_client)?;
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
     // Wrap the connection in a smart pointer
     let db_pool = web::Data::new(db_pool);
+    let email_clint = web::Data::new(email_client);
+
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
@@ -17,6 +77,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .route("/subscriptions", web::post().to(subscribe))
             // Get a pointer copy and attach it to the application state
             .app_data(db_pool.clone())
+            .app_data(email_clint.clone())
     })
     .listen(listener)?
     .run();
