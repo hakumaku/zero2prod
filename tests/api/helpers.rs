c5cae4be8d249e8680c5649fb81f24c14d76d5ca
haku <gentlebuuny@gmail.com>
2024-04-07 14:37:49 +0900

    feat: naive password

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -37,8 +37,10 @@ impl TestApp {
     }
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+        let (username, password) = self.test_user().await;
         reqwest::Client::new()
             .post(&format!("{}/newsletters", &self.address))
+            .basic_auth(username, Some(password))
             .json(&body)
             .send()
             .await
@@ -69,6 +71,15 @@ impl TestApp {
 
         ConfirmationLinks { html, plain_text }
     }
+
+    pub async fn test_user(&self) -> (String, String) {
+        let row = sqlx::query!("SELECT username, password FROM users LIMIT 1")
+            .fetch_one(&self.db_pool)
+            .await
+            .expect("Failed to create test users.");
+
+        (row.username, row.password)
+    }
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
@@ -87,6 +98,18 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to migrate the database");
 
     conn_pool
+}
+
+async fn add_test_user(pool: &PgPool) {
+    sqlx::query!(
+        "INSERT INTO users (user_id, username, password) VALUES ($1, $2, $3)",
+        Uuid::new_v4(),
+        Uuid::new_v4().to_string(),
+        Uuid::new_v4().to_string(),
+    )
+    .execute(pool)
+    .await
+    .expect("Failed to create test users.");
 }
 
 pub async fn spawn_app() -> TestApp {
@@ -125,10 +148,13 @@ pub async fn spawn_app() -> TestApp {
     let address = format!("http://localhost:{}", application_port);
     let _ = tokio::spawn(app.run_until_stopped());
 
-    TestApp {
+    let test_app = TestApp {
         address,
         port: application_port,
         db_pool: get_connection_pool(&config.database),
         email_server,
-    }
+    };
+    add_test_user(&test_app.db_pool).await;
+
+    test_app
 }
