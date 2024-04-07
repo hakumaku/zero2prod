bd2f81a3399c7ac30a6eade8c7233b060a9e3765
haku <gentlebuuny@gmail.com>
2024-04-07 10:49:22 +0900

    feat: handle errors

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,3 +1,4 @@
+use std::fmt::Debug;
 use wiremock::{matchers::method, matchers::path, Mock, ResponseTemplate};
 
 use crate::helpers::spawn_app;
@@ -126,4 +127,20 @@ async fn subscriber_sends_a_confirmatoin_email_with_a_link() {
     let confirmation_links = app.get_confirmation_links(&email_request);
 
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
+}
+
+#[tokio::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // sabotage the database
+    sqlx::query!("ALTER TABLE subscription_tokens DROP COLUMN subscription_token;")
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    let response = app.post_subscriptions(body.into()).await;
+
+    assert_eq!(response.status().as_u16(), 500);
 }
